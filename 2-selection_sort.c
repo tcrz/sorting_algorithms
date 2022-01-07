6a432a3c580069d67e5f1fb7d855c97d8a8eaa1d
Tonycrz <tonybaidoo0@gmail.com>
2022-01-07 19:57:33 +0000

    betty fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,20 +8,21 @@
 
 void selection_sort(int *array, size_t size)
 {
-  size_t i, j, min_idx;
-  int temp;
-  for(i=0; i<size-1; i++)
-    {
-      min_idx = i;
-      for(j=i+1; j<size; j++)
-	if (array[j] < array[min_idx])
-	  min_idx = j;
-      if (i != min_idx)
+	size_t i, j, min_idx;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
 	{
-	  temp = array[min_idx];
-	  array[min_idx] = array[i];
-	  array[i] = temp;
-	  print_array(array, size);
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		if (i != min_idx)
+		{
+			temp = array[min_idx];
+			array[min_idx] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
 	}
-    }
 }
