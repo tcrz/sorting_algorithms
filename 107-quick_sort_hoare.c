55c73bbf8ce0dc4479b6f048b755208dc89a5254
Tonycrz <tonybaidoo0@gmail.com>
2022-01-11 15:12:05 +0000

    quicksort hoare partition

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,78 @@
+#include "sort.h"
+
+/**
+* swap - swaps two values
+* @a: int array
+* @n: size of array
+* @x: value to be swapped
+* @y: value to be swapped
+*/
+void swap(int a[], size_t n, int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+	if (*x != *y)
+		print_array(a, n);
+}
+
+/**
+* partition_hoare - partitions using hoare method
+* with a pivot splitting the two sides
+* @a: array
+* @n: size of array
+* @lo: first value of array
+* @hi: last value of array
+* Return: pivot index
+*/
+int partition_hoare(int a[], size_t n, int lo, int hi)
+{
+	int pivot = a[hi];
+	int i = lo;
+	int j = hi;
+
+	while (i < j)
+	{
+		while (a[i] < pivot)
+			i++;
+
+		while (a[j] > pivot)
+			j--;
+		if (i >= j)
+			return (i);
+		swap(a, n, &a[i], &a[j]);
+		i++;
+		j--;
+	}
+	return (i);
+}
+
+
+/**
+* qs - performs quicksort with hoare partition
+* @arr: array
+* @n: size of array
+* @low: first value of array
+* @high: last value of array
+*/
+void qs(int arr[], size_t n, int low, int high)
+{
+	if (low < high)
+	{
+		int pivot = partition_hoare(arr, n, low, high);
+
+		qs(arr, n, low, pivot - 1);
+		qs(arr, n, pivot, high);
+	}
+}
+
+/**
+* quick_sort_hoare - calls quicksort_proper with required arguments
+* @array: array
+* @size: size of array
+*/
+void quick_sort_hoare(int array[], size_t size)
+{
+	qs(array, size, 0, size - 1);
+	print_array(array, size);
+}
